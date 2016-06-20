26e66fe5645028d49a25cffe0d6dc5e88dd5a3ab
Ashish Kotnala <ashishkotnala1.ak@gmail.com>
2016-06-21 00:34:24 +0530

    fixed multiple declaration/definitions of functions

--- drivers/power/qpnp-charger-dior.c
+++ drivers/power/qpnp-charger-dior.c
@@ -5768,18 +5768,18 @@ static struct spmi_driver qpnp_charger_driver = {
  * qpnp_chg_init() - register spmi driver for qpnp-chg
  */
 int __init
-qpnp_chg_init(void)
+qpnp_chg_init_dior(void)
 {
 	return spmi_driver_register(&qpnp_charger_driver);
 }
-module_init(qpnp_chg_init);
+module_init(qpnp_chg_init_dior);
 
 static void __exit
-qpnp_chg_exit(void)
+qpnp_chg_exit_dior(void)
 {
 	spmi_driver_unregister(&qpnp_charger_driver);
 }
-module_exit(qpnp_chg_exit);
+module_exit(qpnp_chg_exit_dior);
 
 
 MODULE_DESCRIPTION("QPNP charger driver");
